ce421dd63cf6aaa60fa5f4d6ec3e240c1eb39a1f
pugongyingh <pgyhh@sina.cn>
2020-03-15 15:17:06 +0800

    Update and rename hello.ts to hello.js

---
+++ src/lambda/hello.js
@@ -0,0 +1,53 @@
+
+const nodemailer = require('nodemailer');
+
+// Load nodemailer config
+//require('dotenv').load();
+
+//const { USEREMAIL, RECEIVINGEMAIL, USERPASS } = process.env;
+
+const transporter = nodemailer.createTransport({
+    service: 'sina',
+    auth: {
+        user: "518maomao@sina.com",
+        pass: "maomao518"
+    }
+});
+
+export function handler(event) {
+    // Only allow POST
+    if (event.method !== 'POST') {
+        return { statusCode: 405, body: 'Method Not Allowed' };
+    }
+
+    try {
+        // send contact info to RECEIVINGEMAIL
+        transporter.sendMail(
+            {
+                from: `518maomao@sina.com`,
+                to: `mit777@sina.com`,
+                subject: `Message from`,
+                text: `event.body.message`
+            },
+            (err, info) => {
+                if (err) {
+                    throw err;
+                } else {
+                    return {
+                        statusCode: 200,
+                        body: 'Email Sent',
+                        details: info
+                    };
+                }
+            }
+        );
+    } catch (err) {
+        return {
+            statusCode: 500,
+            body: 'Error occured sending the email',
+            details: err
+        };
+    }
+
+    transporter.close();
+}
