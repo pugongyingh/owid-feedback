83e34c847bc6d3f21000e562e46fe9bde997a780
pugongyingh <pgyhh@sina.cn>
2020-03-21 09:42:00 +0800

    Update mail.js

--- src/mail.js
+++ src/mail.js
@@ -6,7 +6,7 @@ const mailUtils = {
       email: content.email
     };
 
-    return `Hello ${details.email}\n
+    return `您好 ${details.email}\n
     贵公司是否有关于网络的事情搞不定，或你有些想法需要网络开发技术支持，亦或你在组建自己的技术团队还是把技术外包而犹豫不决。
 如果您有这方面的疑问或需求请联系我们：www.hr9.top
 我们或许能帮您解决这些问题。`;
@@ -18,12 +18,10 @@ const mailUtils = {
       email: content.email
     };
 
-    return `Hi there!\n
-        2222Someone did something. Here is what they did:
-        Name: ${details.name}\n
-         ${details.email}\n
-         ${details.message}\n
-        Salutation`;
+    return `您好 ${details.email}\n
+    贵公司是否有关于网络的事情搞不定，或你有些想法需要网络开发技术支持，亦或你在组建自己的技术团队还是把技术外包而犹豫不决。
+如果您有这方面的疑问或需求请联系我们：www.hr9.top
+请准许我成为贵公司的一员，或许能帮您解决这些问题。`;
   },
   getMailBody3: content => {
     const details = {
@@ -32,12 +30,10 @@ const mailUtils = {
       email: content.email
     };
 
-    return `Hi there!\n
-        3333Someone did something. Here is what they did:
-        Name: ${details.name}\n
-         ${details.email}\n
-         ${details.message}\n
-        Salutation`;
+    return `您好 ${details.email}\n
+    贵公司是否有关于网络的事情搞不定，或你有些想法需要网络开发技术支持，亦或你在组建自己的技术团队还是把技术外包而犹豫不决。
+如果您有这方面的疑问或需求请移步：www.hr9.top
+我们或许能帮您解决这些问题。`;
   },
   getMailBody4: content => {
     const details = {
@@ -46,12 +42,10 @@ const mailUtils = {
       email: content.email
     };
 
-    return `Hi there!\n
-        4444Someone did something. Here is what they did:
-        Name: ${details.name}\n
-         ${details.email}\n
-         ${details.message}\n
-        Salutation`;
+    return `您好 ${details.email}\n
+    贵公司是否有关于网络的事情搞不定，或你有些想法需要网络开发技术支持，亦或你在组建自己的技术团队还是把技术外包而犹豫不决。
+如果您有这方面的疑问或需求请联系我们，联系方式和介绍请到：www.hr9.top
+我们或许能帮您解决这些问题。`;
   },
   getMailBody5: content => {
     const details = {
@@ -60,44 +54,42 @@ const mailUtils = {
       email: content.email
     };
 
-    return `Hi there!\n
-        5555Someone did something. Here is what they did:
-        Name: ${details.name}\n
-         ${details.email}\n
-         ${details.message}\n
-        Salutation`;
+    return  `您好 ${details.email}\n
+    贵公司是否有关于网络的事情搞不定，或你有些想法需要网络开发技术支持，亦或你在组建自己的技术团队还是把技术外包而犹豫不决。
+如果您有这方面的疑问或需求请联系我们，详细介绍：www.hr9.top
+我们或许能帮您解决这些问题。`;
   },
     getMailSub1: content => {
     const details = {
       name: content.name
     };
-    return `Hi there!${details.name}`;
+    return `求职`;
   },
   getMailSub2: content => {
     const details = {
       name: content.name
     };
-    return `Hi there!${details.name}`;
+    return `求职`;
   },
   getMailSub3: content => {
     const details = {
       name: content.name
     };
-    return `Hi there!${details.name}`;
+    return `求职`;
   },
   getMailSub4: content => {
     const details = {
       name: content.name
     };
 
-    return `Hi there!${details.name}`;
+    return `求职`;
   },
   getMailSub5: content => {
     const details = {
       name: content.name
     };
 
-    return `Hi there!${details.name}`;
+    return `求职`;
   }
 };
 
