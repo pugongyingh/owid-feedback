42bd3807e01bb64abd6c9f07073943be9292ce66
pugongyingh <pgyhh@sina.cn>
2020-03-21 09:19:35 +0800

    Update mail.js

--- src/mail.js
+++ src/mail.js
@@ -6,12 +6,10 @@ const mailUtils = {
       email: content.email
     };
 
-    return `Hi there!\n
-        1111Someone did something. Here is what they did:
-        Name: ${details.name}\n
-         ${details.email}\n
-         ${details.message}\n
-        Salutation`;
+    return `Hello ${details.email}\n
+    贵公司是否有关于网络的事情搞不定，或你有些想法需要网络开发技术支持，亦或你在组建自己的技术团队还是把技术外包而犹豫不决。
+如果您有这方面的疑问或需求请联系我们：www.hr9.top
+我们或许能帮您解决这些问题。`;
   },
   getMailBody2: content => {
     const details = {
