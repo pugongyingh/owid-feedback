5e220dd2ab8837911fa3b38b14e479ba6016522b
pugongyingh <pgyhh@sina.cn>
2020-03-24 09:33:45 +0800

    Update mail.js

--- src/mail.js
+++ src/mail.js
@@ -30,10 +30,9 @@ const mailUtils = {
       email: content.email
     };
 
-    return `您好 ${details.email}\n
-    贵公司是否有关于网络的事情搞不定，或你有些想法需要网络开发技术支持，亦或你在组建自己的技术团队还是把技术外包而犹豫不决。
-如果您有这方面的疑问或需求请移步：www.hr9.top
-我们或许能帮您解决这些问题。`;
+    return `
+    如您有程序开发，网站，微信小程序设计开发需求，或网络推广方面需要一些技术支持。
+请联系我们：https://pensive-brahmagupta-05c0ba.netlify.com`;
   },
   getMailBody4: content => {
     const details = {
@@ -75,7 +74,7 @@ const mailUtils = {
     const details = {
       name: content.name
     };
-    return `求职`;
+    return `您好`;
   },
   getMailSub4: content => {
     const details = {
